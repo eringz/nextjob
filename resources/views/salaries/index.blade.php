8c2285a8812b91a32d6d3dd8eee213b0923ad15f
unknown <ron.garcia.santos@gmail.com>
2024-07-24 16:36:31 +0800

    finished index career salries front end

--- resources/views/salaries/index.blade.php
+++ resources/views/salaries/index.blade.php
@@ -10,17 +10,29 @@
                     <img class="w-[160px] h-[127px]"  src="{{ Vite::asset('resources/images/handbag.svg')}}" alt="hand bag">
                 </div>
             </div>
-            <div role="search" class="bg-white p-6 border rounded-tl-lg rounded-tr-3xl rounded-b-lg">
-                <form action="">
-                    <div class="p-6">
-                        <label for="job-title">What</label>
-                        <div>
-                            <span>
-                                <input type="text" />
+            <div role="search" class="bg-white p-6 h-[120px]  border rounded-tl-lg rounded-tr-3xl rounded-b-lg">
+                <form action="" class="flex">
+                    <div class="w-[492px] mr-2">
+                        <label class="mb-1 font-semibold" for="job-title">What</label>
+                        <div class="p-1 border rounded-lg has-[:focus]:border-blue has-[:focus]:border-b-4 transition:colors duration-1000">
+                            <span class="flex">
+                                <input class="self-end px-3 py-2 w-full outline-none" type="text" placeholder="Job title" />
                             </span>
                         </div>
+                        <div class="autocomplete-container"></div>
                     </div>
-                    <div></div>
+                    <div class="w-[360px] mr-2">
+                        <label class="mb-1 font-semibold" for="job-title">Where</label>
+                        <div class="p-1 border rounded-lg has-[:focus]:border-blue has-[:focus]:border-b-4 transition:colors duration-1000">
+                            <span class="flex">
+                                <input class="px-3 py-2 self-end outline-none" type="text" placeholder="Job title" />
+                            </span>
+                        </div>
+                        <div class="autocomplete-container"></div>
+                    </div>
+                    <div class="">
+                        <button class="bg-blue mt-6 px-6 py-[12px]  text-white font-semibold rounded-lg">Search</button>
+                    </div>
                 </form>
             </div>
         </div>
