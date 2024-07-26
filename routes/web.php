c03cdde022d709aab005870924758ed934824ef9
unknown <ron.garcia.santos@gmail.com>
2024-07-26 14:06:59 +0800

    'update'

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,17 @@ Route::delete('/signout', [SessionController::class, 'destroy'])->middleware('au
 
 Route::get('/', [JobController::class,'index'])->name('index');
 
+
 Route::get('/companies', function () {
     return view('companies.index');
 });
 
 Route::get('/career/salaries', function () {
     return view('salaries.index');
+});
+
+// Employers
+Route::get('recruitment', function () {
+    return view('employers.index');
 });
 
