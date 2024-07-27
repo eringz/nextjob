bf461b7429cfc7272006a36baef5f0e3c26140b6
unknown <ron.garcia.santos@gmail.com>
2024-07-27 18:27:26 +0800

    eos continue employers blade page

---
+++ resources/views/components/svg/site.blade.php
@@ -0,0 +1,22 @@
+<svg class="mx-auto" height="598" viewBox="0 0 652 598" width="652" xmlns="http://www.w3.org/2000/svg">
+    <path d="m9 1h634c4.418278 0 8 3.581722 8 8v592h-650v-592c0-4.418278 3.581722-8 8-8z" fill="#fff"/>
+    <path d="m650 600v-592c0-3.3137085-2.686292-6-6-6h-636c-3.3137085 0-6 2.6862915-6 6v592h-2v-592c0-4.418278 3.581722-8 8-8h636c4.418278 0 8 3.581722 8 8v592z" fill="#ececec"/>
+    <rect fill="#f60" height="45" rx="8" width="136" x="48" y="150"/><rect fill="#fff" height="5" opacity=".2" rx="1" width="74" x="79" y="170"/>
+    <rect fill="#666" height="8" opacity=".5" rx="3" width="200" x="48" y="274.5"/>
+    <g fill="#b2b2b2">
+        <rect height="8" opacity=".3" rx="3" width="520" x="48" y="302.8"/>
+        <rect height="8" opacity=".3" rx="3" width="460" x="48" y="321.6"/>
+        <rect height="8" opacity=".3" rx="3" width="472" x="48" y="340.4"/>
+    </g>
+    <rect fill="#666" height="8" opacity=".5" rx="3" width="200" x="48" y="398.5"/>
+    <g fill="#b2b2b2"><rect height="8" opacity=".3" rx="3" width="520" x="48" y="426.8"/>
+        <rect height="8" opacity=".3" rx="3" width="460" x="48" y="445.6"/>
+        <rect height="8" opacity=".3" rx="3" width="472" x="48" y="464.4"/>
+    </g>
+    <rect fill="#666" height="8" opacity=".5" rx="3" width="200" x="48" y="526.5"/>
+    <rect fill="#b2b2b2" height="8" opacity=".3" rx="3" width="520" x="48" y="554.8"/>
+    <rect fill="#b2b2b2" height="8" opacity=".3" rx="3" width="460" x="48" y="573.6"/>
+    <rect fill="#b2b2b2" height="8" opacity=".3" rx="3" width="472" x="48" y="592.4"/>
+    <rect fill="#2164f3" height="22" rx="2" width="460" x="48" y="48"/>
+    <rect fill="#2164f3" height="8" rx="3" width="350" x="48" y="82"/>
+</svg>
--- resources/views/employers/index.blade.php
+++ resources/views/employers/index.blade.php
@@ -16,6 +16,7 @@
             </div>
         </div>
     </section>
+
     <section class="mx-[189px] pt-[140px] pb-[80px] h-auto">
         <div class="flex pt-[80px] pb-[128px] text-black/80">
             <div class="p-[32px] mx-4 mt-4 mb-[24px] border rounded-lg">
@@ -54,7 +55,7 @@
             <h2 class="ml-[32px] mb-4 text-[36px] font-bold">Save time and effort in your hiring journey.</h2>
             <p class="ml-[32px] text-[21px]">Finding the best fit for the job shouldn’t be a full-time job. NextJob’s simple and <br> powerful tools let you source, screen, and hire faster.</p>
         </div>
-        <div class="bg-[#FFFFFF] mx-[140px] rounded-lg">
+        <div class="bg-[#FFFFFF] mx-[140px] rounded-t-lg">
             <div class="px-[32px] py-[80px]">
                 <div class="flex justify-between">
                     <div class="flex">
@@ -114,6 +115,91 @@
                         You control your posts 24/7—edit, add, pause, or close them whenever you want. <a class="text-blue font-semibold" href="">Learn more about <br> posting.</a>
                     </span>
                 </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="">
+        <div class="mx-[484px] pt-[160px] text-center text-black/70">
+            <h2 class="mb-4 text-[36px] font-bold">Want to fill your job faster? <br> Sponsor it.</h2>
+            <div>
+                <p class="pt-[60px] pb-[100px] text-lg">Sponsored Jobs are over 4.5X more likely to result in a hire. Pick the daily budget that works for you; pay only for people clicking on your post.
+                    <sup>
+                        <button class="px-1 py-3 text-blue font-semibold">1</button>
+                    </sup>
+                </p>
+            </div>
+        </div>
+        <div class="">
+            <div class="mx-48 h-0">
+                <div class="relative top-[140px] left-[400px] bg-[#085FF7] w-[380px] h-[380px] rounded-full">
+                    <div class="relative pt-7 top-[-20px] left-[240px] bg-white w-[120px] h-[120px] text-center text-4xl font-bold border-8 border-[#085FF7] rounded-full">$</div>
+                    <div class="flex mt-10 mx-20 space-x-3">
+                        <x-svg.human />
+                        <x-svg.human />
+                        <x-svg.human />
+                        <x-svg.human />
+                        {{-- progress to be continue --}}
+                    </div>
+                </div>
+            </div>
+            <x-svg.site  />
+        </div>
+        <div>
+            {{-- progress to be continue --}}
+        </div>
+    </section>
+    
+    <section class="pt-[240px] pb-[156px]">
+        <div class="mx-[189px] text-center text-black/70">
+            <div class="mx-[270px]">
+                <h2 class="mb-4 text-[36px] font-bold">You're in good company.</h2>
+                <p class="pt-[40px] pb-[40px] text-[19px]">Over 3,500,000 companies use Indeed to hire. See why these amazing companies use us as their platform to hire dream talent.</p>
+            </div>
+        </div>
+        <div class="px-[28px] p-4">
+            <div class="grid grid-cols-3 pt-2 pb-[24px] mx-[232px] w-[1000px] text-black/70">
+                <div class="mx-3 border rounded-lg shadow-xl">
+                    <div class="p-[24px]">
+                        <img class="mx-auto" src="{{ Vite::asset('resources/images/mcdonalds.svg') }}" alt="">
+                    </div>
+                    <div class="p-[32px]">
+                        <p class="pb-3">Indeed helps the world’s largest family restaurant business to recruit high quality candidates for its hard-to-fill positions.</p>
+                    </div>
+                </div>
+                <div class="mx-3 border rounded-lg shadow-xl">
+                    <div class="p-[24px]">
+                        <img class="mx-auto" src="{{ Vite::asset('resources/images/university-of-oxford.svg') }}" alt="">
+                    </div>
+                    <div class="p-[32px]">
+                        <p class="pb-3">The second oldest university in the world uses Indeed to draw targeted jobseeker traffic to fill both niche academic positions and corporate support roles.</p>
+                    </div>
+                </div>
+                <div class="mx-3 border rounded-lg shadow-xl">
+                    <div class="p-[24px]">
+                        <img class="mx-auto" src="{{ Vite::asset('resources/images/nokia.svg') }}" alt="">
+                    </div>
+                    <div class="p-[32px]">
+                        <p class="pb-3">The world leader in mobile communications uses Indeed Sponsored Jobs to lower its cost per applicant and cost per hire.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="flex">
+            <div class="flex bg-[#0D2D5E] text-white">
+                <div class="pl-[32px] pr-[48px] self-center place-self-end">
+                    <img class="mx-[15px]" src="{{ Vite::asset('resources/images/quote-mark.svg') }}" alt="">
+                    <h4 class="text-[28px] font-semibold" >I've used other websites in the past for hiring; nothing has ever been this easy, this simple, and this effective."</h4>
+                    <p>Darrell</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="{{ Vite::asset('resources/images/customer-testimonial.jpg') }}" alt="">
+                </div>
             </div>
         </div>
     </section>
