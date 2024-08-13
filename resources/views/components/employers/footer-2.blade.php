7c1bb84bd4fb0022f372f577c70d10c8d303b12d
unknown <ron.garcia.santos@gmail.com>
2024-08-13 16:10:31 +0800

    working on create employer front page

---
+++ resources/views/components/employers/footer-2.blade.php
@@ -0,0 +1,36 @@
+<footer class="flex justify-between p-8 text-black/70 text-xs ">
+    <div>
+        <span>©2024 Indeed</span>
+    </div>
+    <ul class="flex space-x-1 ">
+        <li class="flex space-x-1">
+            <div>
+                <a href="">Cookies privacy and terms</a>
+            </div>
+            <span>-</span>
+        </li>
+        <li class="flex space-x-1">
+            <div>
+                <a href="">Privacy center</a>
+            </div>
+            <span>-</span>
+        </li>
+        <li class="flex space-x-1">
+            <div>
+                <a href="">Security</a>
+            </div>
+            <span>-</span>
+        </li>
+        <li class="flex space-x-1">
+            <div>
+                <a href="">Billing</a>
+            </div>
+            <span>-</span>
+        </li>
+        <li class="flex space-x-1">
+            <div>
+                <a href="">Contact</a>
+            </div>
+        </li>
+    </ul>
+</footer>
